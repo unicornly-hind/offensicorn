daced10b2e0fce8f292309886c90f4ff8c8f9dfd
unicornly-hind <49438984+unicornly-hind@users.noreply.github.com>
2025-04-13 21:57:22 +0200

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+// src/content/config.ts
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.string(),
+    slug: z.string(),
+    cover: z.string().optional()
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
