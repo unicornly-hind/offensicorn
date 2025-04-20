bc619e94e5016e7f77f61376f8388540bd1b8e76
unicornly-hind <49438984+unicornly-hind@users.noreply.github.com>
2025-04-20 16:10:53 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react()],
+  adapter: netlify(),
 });
+
